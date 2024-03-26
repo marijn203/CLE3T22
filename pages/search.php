d477f98338f31fda635dca97f064ebc02db32ad0
Mickey <mickeyveldhuizen@gmail.com>
2024-03-26 14:24:04 +0100

    pages map

--- pages/search.php
+++ pages/search.php
@@ -13,14 +13,14 @@ if (isset($_GET['submit'])) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <script type="application/javascript" src="js/main.js"></script>
+    <link rel="stylesheet" href="../css/style.css">
+    <script type="application/javascript" src="../js/main.js"></script>
     <title>Document</title>
 </head>
 <body>
     <nav>
         <!--        dit is het logo     -->
-        <img src="images/logo.png" alt="Logo">
+        <img src="../images/logo.png" alt="Logo">
         <form action="" method="get">
             <input type="text" id="search" placeholder="Search...">
             <input type="submit" id="submit">
@@ -29,14 +29,14 @@ if (isset($_GET['submit'])) {
 
     <main>
         <!--        dit zijn de fotos voor verschillende zorg instellingen      -->
-        <a href="https://trello.com/b/qZwYaU6s/cle-3"><img src="images/Placeholder.png" alt="Logo"></a>
-        <a href="https://trello.com/b/qZwYaU6s/cle-3"><img src="images/Placeholder.png" alt="Logo"></a>
-        <a href="https://trello.com/b/qZwYaU6s/cle-3"><img src="images/Placeholder.png" alt="Logo"></a>
-        <a href="https://trello.com/b/qZwYaU6s/cle-3"><img src="images/Placeholder.png" alt="Logo"></a>
+        <a href="https://trello.com/b/qZwYaU6s/cle-3"><img src="../images/Placeholder.png" alt="Logo"></a>
+        <a href="https://trello.com/b/qZwYaU6s/cle-3"><img src="../images/Placeholder.png" alt="Logo"></a>
+        <a href="https://trello.com/b/qZwYaU6s/cle-3"><img src="../images/Placeholder.png" alt="Logo"></a>
+        <a href="https://trello.com/b/qZwYaU6s/cle-3"><img src="../images/Placeholder.png" alt="Logo"></a>
     </main>
     <footer>
         <!--        dit is het logo     -->
-        <img src="images/logo.png" alt="Logo">
+        <img src="../images/logo.png" alt="Logo">
         <!--        er kunnen hier eventuele social media links/plaatjes        -->
     </footer>
 </body>
