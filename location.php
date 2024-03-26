71bbd23d279ca0872fd37e3f82477c7c80583db0
Mickey <mickeyveldhuizen@gmail.com>
2024-03-26 11:03:32 +0100

    Location pagina start

---
+++ location.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <!--        dit is het logo     -->
+        <img src="https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Begrippenlijst.svg" alt="Logo">
+        <form action="" method="get">
+            <input type="text" id="search" placeholder="Search...">
+            <input type="submit" id="submit">
+        </form>
+    </nav>
+
+    <main>
+<!--        er moet hier een plattegrond komen met text to speech die uitlegt hoe je ergens komt-->
+    </main>
+
+</body>
+</html>
