421fb33e54d30b2e3957f498cd34d23e74d10977
Mickey <mickeyveldhuizen@gmail.com>
2024-03-26 10:46:15 +0100

    Index html start

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+if (isset($_GET['submit'])) {
+    //hier moeten search results worden gezocht
+}
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <script type="application/javascript" src="js/main.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <img src="https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Begrippenlijst.svg" alt="Logo">
+        <form action="" method="get">
+            <input type="text" id="search" placeholder="Search...">
+            <input type="submit" id="submit">
+        </form>
+    </nav>
+
+    <main>
+        <a href="https://trello.com/b/qZwYaU6s/cle-3"><img src="https://www.vorkaccountants.nl/wp-content/uploads/2018/01/placeholder.png" alt="Logo"></a>
+        <a href="https://trello.com/b/qZwYaU6s/cle-3"><img src="https://www.vorkaccountants.nl/wp-content/uploads/2018/01/placeholder.png" alt="Logo"></a>
+        <a href="https://trello.com/b/qZwYaU6s/cle-3"><img src="https://www.vorkaccountants.nl/wp-content/uploads/2018/01/placeholder.png" alt="Logo"></a>
+        <a href="https://trello.com/b/qZwYaU6s/cle-3"><img src="https://www.vorkaccountants.nl/wp-content/uploads/2018/01/placeholder.png" alt="Logo"></a>
+    </main>
+</body>
+</html>
